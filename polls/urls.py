cfcecf79c70b70f38893ad4c5e3d67799595372b
Otabekismailov <15ismailov1995@gmail.com>
2023-04-05 15:17:38 +0500

    newdata

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from polls.views import homepage, questions_list, questions_detile, vote, results
+from polls.views import homepage, questions_list, questions_detile, vote, HomeListVies, question_add
 
 app_name = 'polls'
 urlpatterns = [
@@ -7,5 +7,8 @@ urlpatterns = [
     path('questions/', questions_list, name='questions_list'),
     path('questions/<int:pk>/', questions_detile, name='questions_detile'),
     path('<int:question_id>/vote/', vote, name='vote'),
-    # path('<int:question_id>/results/', results, name='results'),
+    path('list_of_questions/', HomeListVies.as_view(), name=
+    'list_of_questions'),
+    path('questions/add/', question_add, name='question_add'),
 ]
+
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse_lazy
+from django.views.generic import ListView
 
+from polls.forms import QuestionForm
 from polls.models import Question, Choice
 
 
@@ -40,26 +43,26 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        # print(request.POST)
-        # return render(request, 'polls/results.html', context={'choice': question})
-        results(selected_choice.id)
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        if selected_choice.is_true:
+            messages.add_message(request, messages.SUCCESS, 'Hello world.')
+            return HttpResponse(f"<h1>{selected_choice.fun()}<hr>To'g'ri! Javoblar soni: {selected_choice.votes}</h1>")
+        else:
+            messages.add_message(request, messages.ERROR, 'God Bye.')
+            return HttpResponse(
+                f"<h1>{selected_choice.fun()}<hr> Noto'g'ri! Javoblar soni: {selected_choice.votes}</h1>")
+    # return redirect("polls:questions_list")
 
 
-def results(question_id):
-    question = get_object_or_404(Choice, pk=question_id)
-    print(question.text)
-    # # try:
-    # selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    # print(selected_choice.text)
+class HomeListVies(ListView):
+    model = Question
+    template_name = 'polls/list_of_questions.html'
 
-    # except (KeyError, Choice.DoesNotExist):
-    #     # Redisplay the question voting form.
-    #     return render(request, 'polls/questions_detile.html', {
-    #         'question': question,
-    #         'error_message': "You didn't select a choice.",
-    #     })
-    return render(question_id, 'polls/results.html', context={'choice': question})
+
+def question_add(request):
+    form = QuestionForm(request.POST)
+    if request.method == "POST":
+        success_url = reverse_lazy('polls:homepage')
+        if form.is_valid():
+            form.save()
+            return redirect("polls:questions_list")
+    return render(request, 'polls/add_question.html', {"form": form})
